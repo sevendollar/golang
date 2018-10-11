6c9b8fd7c4521b143bf965cd3aee1738c36ab478
sevendollar <sevendollar@gmail.com>
2018-10-11 02:41:59 +0000

    minor fix of routes.go/process_signup

--- http-exercise-2/routes.go
+++ http-exercise-2/routes.go
@@ -1,131 +1,144 @@
 package main
 
 import (
-        "fmt"
-        "golang.org/x/crypto/bcrypt"
-        "html/template"
-        "net/http"
-        "strconv"
-        "time"
-        "web/data"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"html/template"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+	"web/data"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-        t, err := template.ParseFiles("web/templates/index.html")
-        if err != nil {
-                return
-        }
-        t.Execute(w, "")
+	session_id, err := r.Cookie("SessionId")
+	if err != nil {
+		t, err := template.ParseFiles("/go/src/web/templates/index_guest.html")
+		if err != nil {
+			return
+		}
+		t.Execute(w, "")
+	} else {
+		u := &data.User{}
+		u, err = data.GetUserBySessionUuid(session_id.Value)
+		if err != nil {
+			return
+		}
+		t, err := template.ParseFiles("/go/src/web/templates/index.html")
+		if err != nil {
+			return
+		}
+		t.Execute(w, strings.ToUpper(u.Name))
+	}
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {
-        t, err := template.ParseFiles("web/templates/signup.html")
-        if err != nil {
-                return
-        }
-        t.Execute(w, "")
+	t, err := template.ParseFiles("/go/src/web/templates/signup.html")
+	if err != nil {
+		return
+	}
+	t.Execute(w, "")
 }
 
 func process_signup(w http.ResponseWriter, r *http.Request) {
-        r.ParseForm()
+	r.ParseForm()
 
-        age, err := strconv.Atoi(r.PostFormValue("age"))
-        if err != nil {
-                return
-        }
+	age, err := strconv.Atoi(r.PostFormValue("age"))
+	if err != nil {
+		fmt.Fprintf(w, "something went wrong with Age field, Error: %v", err)
+		return
+	}
 
-        u := &data.User{
-                Name:     r.PostFormValue("name"),
-                Password: r.PostFormValue("password"),
-                Sex:      r.PostFormValue("sex"),
-                Age:      age,
-                Email:    []string{r.PostFormValue("email")},
-        }
-        err = u.Create()
-        if err != nil {
-                fmt.Fprintf(w, "failed: %v", err)
-                return
-        } else {
-                fmt.Fprintf(w, "suceed")
-        }
+	u := &data.User{
+		Name:     r.PostFormValue("name"),
+		Password: r.PostFormValue("password"),
+		Sex:      r.PostFormValue("sex"),
+		Age:      age,
+		Email:    []string{r.PostFormValue("email")},
+	}
+	err = u.Create()
+	if err != nil {
+		fmt.Fprintf(w, "Error: %v", err)
+	} else {
+		fmt.Fprintf(w, "suceed")
+	}
 
 }
 
 func signin(w http.ResponseWriter, r *http.Request) {
-        t, err := template.ParseFiles("web/templates/signin.html")
-        if err != nil {
-                return
-        }
-        t.Execute(w, "")
+	t, err := template.ParseFiles("/go/src/web/templates/signin.html")
+	if err != nil {
+		return
+	}
+	t.Execute(w, "")
 }
 
 func signout(w http.ResponseWriter, r *http.Request) {
-        session_id, err := r.Cookie("SessionId")
-        if err != nil {
-                fmt.Fprintln(w, "you're already sign-outed!")
-                return
-        }
+	session_id, err := r.Cookie("SessionId")
+	if err != nil {
+		fmt.Fprintln(w, "you're already sign-outed!")
+		return
+	}
 
-        s := &data.Session{
-                Uuid: session_id.Value,
-        }
-        err = s.Delete()
-        if err != nil {
-                return
-        }
+	s := &data.Session{
+		Uuid: session_id.Value,
+	}
+	err = s.Delete()
+	if err != nil {
+		return
+	}
 
-        http.SetCookie(w, &http.Cookie{
-                Name:    "SessionId",
-                MaxAge:  -1,
-                Expires: time.Unix(1, 0),
-        })
-        fmt.Fprintln(w, "you're sign-outed!")
+	http.SetCookie(w, &http.Cookie{
+		Name:    "SessionId",
+		MaxAge:  -1,
+		Expires: time.Unix(1, 0),
+	})
+	fmt.Fprintln(w, "you're sign-outed!")
 }
 
 func autheticate(w http.ResponseWriter, r *http.Request) {
-        r.ParseForm()
+	r.ParseForm()
 
-        post_name := r.PostFormValue("name")
-        post_password := r.PostFormValue("password")
-        store_user, err := data.GetUserByName(post_name)
-        if err != nil {
-                fmt.Fprintln(w, "Error:", err)
-                return
-        }
-        hashed_passwod := store_user.Password
+	post_name := r.PostFormValue("name")
+	post_password := r.PostFormValue("password")
+	store_user, err := data.GetUserByName(post_name)
+	if err != nil {
+		fmt.Fprintln(w, "Error:", err)
+		return
+	}
+	hashed_passwod := store_user.Password
 
-        if err := bcrypt.CompareHashAndPassword([]byte(hashed_passwod), []byte(post_password)); err != nil {
-                //failed code goes here
-                fmt.Fprintln(w, "Sign In Failed!, Error:", err)
-        } else {
-                //sucseed code goes here
+	if err := bcrypt.CompareHashAndPassword([]byte(hashed_passwod), []byte(post_password)); err != nil {
+		//failed code goes here
+		fmt.Fprintln(w, "Sign In Failed!, Error:", err)
+	} else {
+		//sucseed code goes here
 
-                session, err := store_user.CreateSession()
-                if err != nil {
-                        fmt.Fprintln(w, "Error:", err)
-                        return
-                }
+		session, err := store_user.CreateSession()
+		if err != nil {
+			fmt.Fprintln(w, "Error:", err)
+			return
+		}
 
-                http.SetCookie(w, &http.Cookie{
-                        Name:     "SessionId",
-                        Value:    session.Uuid,
-                        HttpOnly: true,
-                })
+		http.SetCookie(w, &http.Cookie{
+			Name:     "SessionId",
+			Value:    session.Uuid,
+			HttpOnly: true,
+		})
 
-                fmt.Fprintln(w, "Sign In Succeed!")
-        }
+		fmt.Fprintln(w, "Sign In Succeed!")
+	}
 }
 
 func verify(w http.ResponseWriter, r *http.Request) {
-        r.ParseForm()
+	cookie_map := getCookie(r.Header.Get("cookie"))
+	session_id := cookie_map["SessionId"]
 
-        cookie_map := getCookie(r.Header.Get("cookie"))
-        session_id := cookie_map["SessionId"]
-
-        u, err := data.GetUserBySessionUuid(session_id)
-        if err != nil {
-                fmt.Fprintln(w, "hi guest")
-                return
-        }
-        fmt.Fprintf(w, "hi %v, you're now %v years old.", u.Name, u.Age)
+	u, err := data.GetUserBySessionUuid(session_id)
+	if err != nil {
+		fmt.Fprintln(w, "hi guest")
+		return
+	}
+	fmt.Fprintf(w, "hi %v, you're now %v years old.", u.Name, u.Age)
 }
