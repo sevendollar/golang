68a8944ed96bce0762af704954f5564f4c75ab13
sevendollar <sevendollar@gmail.com>
2018-12-04 14:09:42 +0000

    modify output

--- star/star.go
+++ star/star.go
@@ -1,73 +1,86 @@
 package star
 
 import (
-        "errors"
-        "fmt"
-        "net/http"
-        "strconv"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
 
-        "github.com/PuerkitoBio/goquery"
+	"github.com/PuerkitoBio/goquery"
 )
 
 var (
-        stars = []string{
-                "Aries",
-                "Taurus",
-                "Gemini",
-                "Cancer",
-                "Leo",
-                "Virgo",
-                "Libra",
-                "Scorpio",
-                "Sagittarius",
-                "Capricornus",
-                "Aquarius",
-                "Pisces",
-        }
+	stars = []string{
+		"Aries",
+		"Taurus",
+		"Gemini",
+		"Cancer",
+		"Leo",
+		"Virgo",
+		"Libra",
+		"Scorpio",
+		"Sagittarius",
+		"Capricornus",
+		"Aquarius",
+		"Pisces",
+	}
 )
 
-func GetStar(index ...int) (r string, err error) {
-        getInfo := func(i int) (t string, err error) {
-                ii := strconv.Itoa(i)
-                url := "http://astro.click108.com.tw/daily_" + ii + ".php?iAstro=" + ii
-                resp, err := http.Get(url)
-                if err != nil {
-                        return
-                }
-                defer resp.Body.Close()
+func GetStar(starIndex []int, dayIndex int) (r string, err error) {
+	getInfo := func(i int, dayIndex int) (t string, err error) {
+		yy, mm, dd := time.Now().Add(time.Duration(dayIndex) * 24 * time.Hour).Date()
+		date := strconv.Itoa(yy) + "-" + strconv.Itoa(int(mm)) + "-" + strconv.Itoa(dd)
 
-                if resp.StatusCode != 200 {
-                        err = fmt.Errorf("status code error: %d %s", resp.StatusCode, resp.Status)
-                        return
-                }
+		ii := strconv.Itoa(i)
+		url := "http://astro.click108.com.tw/daily_" + ii + ".php?iAstro=" + ii + "&iType=0&iAcDay=" + date
+		resp, err := http.Get(url)
+		if err != nil {
+			return
+		}
+		defer resp.Body.Close()
 
-                doc, err := goquery.NewDocumentFromReader(resp.Body)
-                if err != nil {
-                        return
-                }
+		if resp.StatusCode != 200 {
+			err = fmt.Errorf("status code error: %d %s", resp.StatusCode, resp.Status)
+			return
+		}
 
-                t += stars[i] + ":\n"
-                doc.Find("div[class=TODAY_CONTENT] p").Each(func(i int, s *goquery.Selection) {
-                        t += s.Text() + "\n"
-                })
-                return
-        }
+		doc, err := goquery.NewDocumentFromReader(resp.Body)
+		if err != nil {
+			return
+		}
 
-        if len(index) == 0 {
-                err = errors.New("add some indexes")
-                return
-        }
-        var tmp string
-        for i, v := range index {
-                tmp, err = getInfo(v)
-                if err != nil {
-                        return
-                }
-                r += tmp
-                if i != len(index)-1 {
-                        r += "\n"
-                }
-        }
-        return
+		t += "【" + stars[i] + "】\n"
+		t1 := []string{}
+		t2 := ""
+		doc.Find("div[class=TODAY_CONTENT] p").Each(func(i int, s *goquery.Selection) {
+			t1 = append(t1, s.Text())
+		})
+		for i := 0; i < 8; i++ {
+			if i%4 == 0 {
+				t += t1[i][:9] + t1[i][12:len(t1[i])-3] + "    " + t1[i+2][:9] + t1[i+2][12:len(t1[i])-3] + "\n"
+			} else if i%2 != 0 {
+				t2 += fmt.Sprintf("%v:\n%v\n", t1[i-1][:9], t1[i])
+			}
+		}
+		t += t2
+		return
+	}
+
+	if len(starIndex) == 0 {
+		err = errors.New("add some indexes")
+		return
+	}
+	var tmp string
+	for i, v := range starIndex {
+		tmp, err = getInfo(v, dayIndex)
+		if err != nil {
+			return
+		}
+		r += tmp
+		if i != len(starIndex)-1 {
+			r += "\n"
+		}
+	}
+	return
 }
-
